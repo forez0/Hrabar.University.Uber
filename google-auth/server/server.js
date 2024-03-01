8af281f76c3ae11a6e0b0c847041b8541b023425
danylo <danylohrabar@gmail.com>
2024-03-01 18:06:14 +0200

    start + server file

---
+++ google-auth/server/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+
+// importing dotenv module for accesing and enviroment variables//
+
+app.use(
+    cookieSession({
+        name: "session",
+        keys: ["danylo"],
+        maxAge: 24 * 60 * 60 * 100,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+    })
+);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
