03a423ea3d4f70514c7a684d0d8819b1b6533694
danylo <danylohrabar@gmail.com>
2024-03-03 17:43:11 +0200

    login and sign up in pages

--- google-auth/server/client/src/App.js
+++ google-auth/server/client/src/App.js
@@ -1,3 +1,9 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import Login from "./pages/Login"
+import Login from "./pages/SignUp"
+
 import './App.css';
 
 function App() {
