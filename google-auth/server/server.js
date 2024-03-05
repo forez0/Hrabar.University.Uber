89e88cfce4f92903883d7ef788cae140ace476ea
danylo <danylohrabar@gmail.com>
2024-03-05 10:25:40 +0200

    just trying to run google authentication (another fail..)

--- google-auth/server/server.js
+++ google-auth/server/server.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5500",
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     })
